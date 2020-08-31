b1d418a10e30cfd988b6f8a17250a50476895929
madelinelise <madelinelise@gmail.com>
2020-08-31 19:58:51 -0400

    Creates body text style sheet.

--- src/components/body-text/body-text.js
+++ src/components/body-text/body-text.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./body-text.scss";
 
 export const BodyText = (props) => {
   const { description } = props;
--- src/components/body-text/body-text.stories.js
+++ src/components/body-text/body-text.stories.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { BodyText } from "./body-text";
-import data from './data.json';
+import data from "./data.json";
 
-// This default export determines where you story goes in the story list
 export default {
   title: "Components/BodyText",
   component: BodyText,
