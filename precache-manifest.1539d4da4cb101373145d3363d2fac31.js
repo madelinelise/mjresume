badd39d6f64f6c0c3b40206ba15546d0ac640acd
madelinelise <madelinelise@gmail.com>
2020-12-17 11:35:22 -0500

    Updates

---
+++ precache-manifest.1539d4da4cb101373145d3363d2fac31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6c1c6ab0a165e7cdd7526d67f39e42",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "269c81f3b684be30945d",
+    "url": "/resume/static/css/main.4cbe1b73.chunk.css"
+  },
+  {
+    "revision": "d9086dcb52abf58d446a",
+    "url": "/resume/static/js/2.d1fa9980.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.d1fa9980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "269c81f3b684be30945d",
+    "url": "/resume/static/js/main.a4cc41df.chunk.js"
+  },
+  {
+    "revision": "d9665f3b829e996cfaa8",
+    "url": "/resume/static/js/runtime-main.5c15d340.js"
+  }
+]);
